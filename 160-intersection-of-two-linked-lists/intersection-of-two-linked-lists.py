7367972bcb40fe6a0c1667de5d99ceb318ed2d27
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-04-04 13:15:21 +0530

    Time: 128 ms (6.04%) | Memory: 38.1 MB (38.89%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -8,11 +8,29 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         p1=headA
         p2=headB
+        lenA=0
+        lenB=0
+        # solving using length 
+        while p1!=None: 
+            lenA+=1
+            p1=p1.next
         
-        while p1!=p2:
-            # move pointer to next
-            p1=p1.next if p1 else headB
-            p2=p2.next if p2 else headA
-        return p1      
+        while p2!=None:
+            lenB+=1
+            p2=p2.next
+        p1=headA
+        p2=headB    
+        if lenA>lenB:
+            for _ in range(lenA-lenB):
+                p1=p1.next
+        else:
+            for _ in range(lenB-lenA):
+                p2=p2.next
+        while p1 and  p2:
+            if p1==p2:
+                return p1
+            p1=p1.next
+            p2=p2.next
+        return None        
 
                   
