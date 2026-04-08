4f48749230c768b13789d8188cbeed93f9b8398e
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-04-08 21:37:20 +0530

    Time: 51 ms (15.64%) | Memory: 42.5 MB (65.86%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+      def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow= head
+        fast=head
+        
+        prev=None
+        while fast!=None and fast.next!=None: # getting access at half in the list
+            fast=fast.next.next
+            slow=slow.next    
+        
+        while slow!=None:  # reversing the half of the linkesd list
+            new_node=slow.next
+            slow.next=prev
+            prev=slow
+            slow=new_node
+        first=head
+        second=prev
+        while second:  # comparing the both list
+            if first.val!=second.val:
+                return False
+            first=first.next
+            second=second.next
+        return True        
+            # time complexity =o(n)+o(n)=o(n)
+            # space complexity=o(1)
