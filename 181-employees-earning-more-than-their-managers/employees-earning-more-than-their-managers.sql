3c96ca6961bbddaf5ea7aabbb95867bd54500911
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-06-19 18:42:38 +0530

    Time: 384 ms (68.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT
+    E.name AS Employee
+FROM Employee E
+JOIN Employee M
+    ON E.managerId = M.id
+WHERE E.salary > M.salary; # condition to get more  salary than manager
