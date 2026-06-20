7856293cf33992173039f77b9128fc7309661beb
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-06-20 18:28:24 +0530

    Time: 390 ms (95.76%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select 
-P.firstname,P.lastname,a.city,a.state #selecting the require column
- from Person p # person as p
-left join   Address a # address as a and left join
-on p.personid=a.personid #join on personid
+Person.firstname,Person.lastname,Address.city,Address.state #selecting the require column
+ from Person  
+left join Address 
+on Person.personid=Address.personid #join on personid
 
