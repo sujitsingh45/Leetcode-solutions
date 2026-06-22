b9a7249f686bfc527e31e4735bdb25357d359a62
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-06-22 19:39:03 +0530

    Time: 900 ms (37.28%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement
 DELETE p1
 FROM Person p1
-JOIN Person p2  # SELF JOINING
+JOIN Person p2 #SELF JOINING
 ON p1.email = p2.email
-WHERE p1.id > p2.id; # delete whose id is greater
+WHERE p1.id > p2.id; #delete whoose id is greater
