c224e62b1f66e13af5642b35874c652818a7c70e
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-06-23 18:39:30 +0530

    Time: 815 ms (5.74%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select Customers.name as Customers #taking column name as Customers 
-from Customers 
+from Customers # from customers table
 left join Orders #left join with orders table  
 on Customers.id=Orders.customerId
 where Orders.customerId is null; #extracting the one who orders
