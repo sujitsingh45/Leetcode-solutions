b94c389237677c63b8d3943f41e88b23b729586c
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-06-26 19:17:01 +0530

    Time: 941 ms (91.74%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select e.name,b.bonus
- from Employee e
- left join Bonus b
- on e.empId=b.empId
- where b.bonus < 1000
+select e.name,b.bonus #selecting require column
+ from Employee e 
+ left join Bonus b #right join on bonus table
+ on e.empId=b.empId #joining by empid
+ where b.bonus < 1000 # condition
   or b.bonus is null;
