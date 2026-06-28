46b366d4f44e360257636adab248389ae6747702
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-06-28 17:40:32 +0530

    Time: 1043 ms (22.30%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* # Write your MySQL query statement below
 SELECT
     d.name AS Department, #selecting required column as per need 
     e.name AS Employee,
@@ -13,3 +13,17 @@ JOIN (
 )s
     ON e.departmentId = s.departmentId
    AND e.salary = s.max_salary; #getting max salary by department
+
+    */
+select 
+d.name as Department,# selecting required column as per needed name
+e.name as Employee,
+e.salary as Salary
+from Employee e
+join Department d #inner join 
+on e.departmentId =d.id #inner join on department id
+where e.salary=(  #subquery
+    select max(salary) as max_salary #getting max salary
+    from Employee 
+    where departmentid=e.departmentid #returning only one max rows
+);
