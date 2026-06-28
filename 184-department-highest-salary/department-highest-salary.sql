9a74ae77e215d1ec37b61424d76a63bf509c2a8d
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-06-28 17:31:52 +0530

    Time: 1017 ms (27.10%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
 select 
-d.name as Department,
+d.name as Department,# selecting required column as per needed name
 e.name as Employee,
 e.salary as Salary
 from Employee e
-join Department d
-on e.departmentId =d.id
-where e.salary=(
-    select max(salary) as max_salary
+join Department d #inner join 
+on e.departmentId =d.id #inner join on department id
+where e.salary=(  #subquery
+    select max(salary) as max_salary #getting max salary
     from Employee 
-    where departmentid=e.departmentid
+    where departmentid=e.departmentid #returning only one max rows
 );
