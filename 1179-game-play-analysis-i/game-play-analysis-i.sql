f26473ddfb7dd12be9388bb9c4a00ea60b0344de
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-07-01 22:54:50 +0530

    Time: 2356 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select player_id,
-min(event_date) as first_login #return the earliest date and changing to first_login
+min(event_date) as first_login#return the earliest date and changing to first_login
 from Activity 
-group by player_id; #group by player_id
+group by player_id;#group by player_id
