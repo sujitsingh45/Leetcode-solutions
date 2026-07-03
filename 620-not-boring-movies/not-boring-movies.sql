1e5ec51278e2d1ffae58a54420f40b31615597a0
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-07-03 19:57:25 +0530

    Time: 313 ms (13.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select 
+id,movie,description,rating # selecting required column 
+from Cinema
+where id%2!=0 and description != "boring" #condition not even and boring 
+order by rating DESC; #table in dsecending order
