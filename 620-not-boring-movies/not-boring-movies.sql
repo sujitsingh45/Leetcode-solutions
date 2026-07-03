cb3f0766cf562102b12d726c1aed05d10391552f
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-07-03 19:59:01 +0530

    Time: 293 ms (24.75%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -3,4 +3,4 @@ select
 id,movie,description,rating # selecting required column 
 from Cinema
 where id%2!=0 and description != "boring" #condition not even and boring 
-order by rating DESC; #table in dsecending order
+order by rating DESC; #table in dsecending order 
