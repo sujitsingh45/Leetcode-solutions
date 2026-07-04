4bff5c6ae0363c50ca987d7478fd1c0e26b47fd3
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-07-04 10:18:24 +0530

    Time: 571 ms (71.02%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -2,8 +2,7 @@
 SELECT customer_id
 FROM Customer
 GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) =
-
+HAVING COUNT(DISTINCT product_key) = #conditon that all product key should be there 
 (
     SELECT COUNT(*)
     FROM Product
