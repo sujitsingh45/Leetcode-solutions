a76a064bc4fd59629e2fa33690383513e0ef20ec
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-07-08 22:34:18 +0530

    Time: 463 ms (36.66%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT MAX(num) AS num
+FROM MyNumbers #selecting maximum form those count one
+WHERE num IN (  
+    # selecting single count number
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+);
+
+
