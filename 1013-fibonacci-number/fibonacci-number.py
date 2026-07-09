3b8f2719c18ff1805b966fe56cf1221a1d220a20
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-07-09 20:03:40 +0530

    Time: 663 ms (18.47%) | Memory: 12.5 MB (16.21%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,7 +4,7 @@ class Solution(object):
         if n==0 or n==1:
             return n
         #recursion call
-        return self.fib(n-1)+self.fib(n-2)         
+        return self.fib(n-2)+self.fib(n-1)         
             
                  
 
