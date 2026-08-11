817405d3133af996fef4d706c15fbd51742ea6b0
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-08-11 17:43:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.06%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -1,13 +1,23 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        #using mathematical solution
         if num<10:
+            return num
+        return (num-1)%9 +1
+
+        #time complexity:O(1)
+        #space complexity:O(1)
+
+
+
+        
+        '''if num<10:
             return num
         d_sum=0 #to return the sum of the digit    
         while num>0:
             d_sum+=num%10
             num//=10 # floor divison to get  next unit digit
             
-        return self.addDigits(d_sum)    
-
+        return self.addDigits(d_sum) '''
 
         
