3d43c91e08aa1210183da140519d2ae1f95ffe2d
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-08-17 19:04:42 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (17.62%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self,nums:List[int],target:int)->List[int]:
+       n=len(nums)
+       dict1={}
+       for i in range(n):
+            rem=target-nums[i]
+            if rem in dict1: #return if present
+                return [dict1[rem],i]
+            dict1[nums[i]]=i
+
+          #time complexity: O(n)
+          #space complexity: O(n)  
+        
