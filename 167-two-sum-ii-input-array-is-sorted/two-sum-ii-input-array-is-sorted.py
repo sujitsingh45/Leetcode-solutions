ad30854efec919e84e9755d29d8c7ee16dfae1cf
sujitsingh45 <sujitkumarstm1114@gmail.com>
2026-08-17 19:43:13 +0530

    Time: 4 ms (49.36%) | Memory: 20.4 MB (95.35%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''n=len(numbers)
+        di_ct={}
+        for i in range(n):
+            rem=target-numbers[i]
+            if rem in di_ct:
+                return [di_ct[rem]+1,i+1]
+            di_ct[numbers[i]]=i '''
+        #using two pointers
+        left=0
+        right=len(nums)-1
+        while left<right:
+            sum1=nums[left]+nums[right]
+            if sum1==target: #return if equal to target
+                return [left+1,right+1] 
+            elif sum1>target:  #if greater deacrement right 
+                right-=1
+            else: # else increment left
+                left+=1        
+
+     #time complexity:O(n)
+     #space complexity:O(1)
+        
